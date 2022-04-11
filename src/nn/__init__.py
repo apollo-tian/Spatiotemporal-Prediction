24413d785eaa58d4491b5f3c714764f571bc22c8
644476114@qq.com <644476114@qq.com>
2022-04-11 16:17:09 +0800

    修改模块导入

--- src/nn/__init__.py
+++ src/nn/__init__.py
@@ -1,8 +1,3 @@
-# !/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @time: 2022/4/9 20:10
-# @author: 芜情
-# @description:
 from .EnhancedModule import EnhancedModule
 from .ConvLSTM import ConvLSTM
 
